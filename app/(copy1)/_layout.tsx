f6c67dd936ce51c354e71fc47694bd91835e596b
Kyungmin1995 <rudals782@nate.com>
2024-11-26 21:23:25 +0900

    copy1 commit

---
+++ app/(copy1)/_layout.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Stack } from "expo-router";
+import Header from "@/components/Header";
+import { Platform, SafeAreaView, View } from "react-native";
+
+export default function StackLayout() {
+  return (
+    <>
+      <Stack
+        screenOptions={{
+          animation: "none",
+        }}
+      >
+        <Stack.Screen
+          name="index"
+          options={{
+            title: "관심 목록",
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="post"
+          options={{
+            title: "내가 쓴 글/댓글",
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="chat"
+          options={{
+            title: "채팅",
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="profile"
+          options={{
+            title: "프로필 설정",
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="detail"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </>
+  );
+}
---
+++ app/_layout.tsx
@@ -0,0 +1,45 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { Text, View } from "react-native";
+import "react-native-reanimated";
+
+//스플래시 스크린 자동끄기
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    ...FontAwesome.font,
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  return (
+    <>
+      <StatusBar />
+      <Stack>
+        <Stack.Screen name="(copy1)" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  );
+}
